50bd6391d133ec3067631d3973ac6e96f9e523d0
mind0bender <mind0bender@gmail.com>
2024-08-19 14:18:46 +0530

    { "C": ["merge sort 4.1"] }

---
+++ practical4/practical4.1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void TopDownMerge(int B[], int iBegin, int iMiddle, int iEnd, int A[]) {
+  int i = iBegin, j = iMiddle;
+
+  for (int k = iBegin; k < iEnd; k++) {
+    if (i < iMiddle && (j >= iEnd || A[i] <= A[j])) {
+      B[k] = A[i];
+      i = i + 1;
+    } else {
+      B[k] = A[j];
+      j = j + 1;
+    }
+  }
+}
+
+void TopDownSplitMerge(int B[], int iBegin, int iEnd, int A[]) {
+  if (iEnd - iBegin <= 1) return;
+
+  int iMiddle = (iEnd + iBegin) / 2;
+
+  TopDownSplitMerge(A, iBegin, iMiddle, B);
+  TopDownSplitMerge(A, iMiddle, iEnd, B);
+
+  TopDownMerge(B, iBegin, iMiddle, iEnd, A);
+}
+
+void TopDownMergeSort(int A[], int B[], int n) {
+  TopDownSplitMerge(A, 0, n, B);
+}
+
+int main() {
+  int n;
+  cout << "Enter no. of elements: ";
+  cin >> n;
+
+  int A[n];
+  int B[n];
+  cout << "Enter " << n << " elements: ";
+  for (int i = 0; i < n; i++) {
+    int ele;
+    cin >> ele;
+    A[i] = ele;
+    B[i] = ele;
+  }
+
+  TopDownMergeSort(A, B, n);
+
+  cout << "Sorted elements are: ";
+  for (int ele : A) {
+    cout << ele << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}
